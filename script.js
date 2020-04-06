7bad597a696dd973a250db7f8960bdbf9fefe7f4
aaronhalstonlee <aaronhalstonlee@gmail.com>
2020-04-06 11:05:59 -0700

    added random color logic

--- script.js
+++ script.js
@@ -18,13 +18,18 @@ function generateDiv(v){
             var cell = document.createElement('div');
             cell.className = "cell";
             cell.id = "box";
-            cell.style.height = (cellSize-2)+"px";
-            cell.style.width = (cellSize-2)+"px";
+            cell.style.height = (cellSize-1)+"px";
+            cell.style.width = (cellSize-1)+"px";
             cell.addEventListener("mouseenter", (e) => {
-                e.target.className = "red";
+                e.target.style.backgroundColor = "red";
             });
             cell.addEventListener("mouseleave", (e) => {
-                e.target.className = "draw";
+                if(randomColor){
+                    e.target.style.backgroundColor = randColor();
+                }
+                if(!randomColor){
+                    e.target.style.backgroundColor = "black";
+                }
             })
             cell.addEventListener("click", (e) => {
                 e.target.className = "cell";
@@ -35,6 +40,17 @@ function generateDiv(v){
     }
 }
 generateDiv(gridSize);
+//
+
+//random color generator
+function randColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 //
 
 //clear button
@@ -48,6 +64,7 @@ button.addEventListener("click", (e) => {
   canvas.innerHTML = "";
   gridSize = prompt('how big a grid you want, yo?')
   generateDiv(gridSize);
+  randomColor = false;
 })
 //
 
